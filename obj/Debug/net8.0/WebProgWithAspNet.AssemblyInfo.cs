7ca55db57b644de05800f169f32809a51c40ee13
kocaemre <emrekoca2003@gmail.com>
2024-11-28 14:11:45 +0300

    edited index page

--- obj/Debug/net8.0/WebProgWithAspNet.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebProgWithAspNet.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebProgWithAspNet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80d4dc1076f7f1039a8d8f64312061b3ca0a9578")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebProgWithAspNet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebProgWithAspNet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
